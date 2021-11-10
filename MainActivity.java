68843df0fceca418e574eabef2319fee7d7bb3a8
tedd-E <62875128+tedd-E@users.noreply.github.com>
2021-11-09 18:54:51 -0800

    database example
    writing/reading user database example

--- MainActivity.java
+++ MainActivity.java
@@ -1,73 +1,91 @@
-package com.example.firebasedemo;
+package com.example.getuserinput;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.*;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.*;
-import android.view.*;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.*;
+import com.google.firebase.firestore.auth.User;
 
 public class MainActivity extends AppCompatActivity {
 
-    int clicks = 0;
-    private Button button;
-    private DatabaseReference fireBase;
+    private static final String TAG = "egg";
+    String name, email;
+    int user = 0;
+    EditText nameInput, emailInput;
+    Button submitButton, readButton;
 
-    class PlaceHolder {
-        String name;
-        int followers;
-        PlaceHolder(String name, int followers) {
-            this.name = name;
-            this.followers = followers;
-        }
-    }
+    private FirebaseDatabase db = FirebaseDatabase.getInstance();
+    private DatabaseReference root = db.getReference().child("Users");
+
+    //get from database
+    private FirebaseDatabase rb = FirebaseDatabase.getInstance();
+    private DatabaseReference dataReference = rb.getReference();
+    private ListView mListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        HashMap<String, Object> userOne = new HashMap<>(); //user class
-        userOne.put("Name", "User One");
-        userOne.put("Gym", "AVAC");
-        userOne.put("Bio", "Hello, I'm User One");
-        userOne.put("Sports preference", "Track");
+        nameInput = (EditText) findViewById(R.id.nameInput);
+        emailInput = (EditText) findViewById(R.id.emailInput);
+        submitButton = (Button)findViewById(R.id.button);
 
-        HashMap<String, Object> userTwo = new HashMap<>();
-        userTwo.put("Name", "User Two");
-        userTwo.put("Gym", "RIMAC");
-        userTwo.put("Bio", "Hi, I'm User Two");
-        userTwo.put("Sports preference", "Swimming");
-        FirebaseDatabase.getInstance().getReference().child("FireBaseTriDevDemo").child("1").updateChildren(userOne);
-        FirebaseDatabase.getInstance().getReference().child("FireBaseTriDevDemo").child("2").updateChildren(userTwo);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name = nameInput.getText().toString();
+                email = emailInput.getText().toString();
+
+                HashMap<String, String> usermap = new HashMap<>();
+                usermap.put("Name", name);
+                usermap.put("Email", email);
+
+                root.child("" + ++user).setValue(usermap);
+            }
+        });
+
+//ok up to this point
+
+        dataReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                showData(snapshot);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
-        List<PlaceHolder> userList = new ArrayList<>();
-        for(int i = 1; i < 11; i++)
-            userList.add(new PlaceHolder("User " + i, 10 + i));
-
-        UserProfile emptyUser = new UserProfile();
-        FirebaseDatabase.getInstance().getReference().child("FireBaseTriDevDemo").child("Android").setValue(emptyUser);
-        fireBase = FirebaseDatabase.getInstance().getReference();
-        newUser("User Zero", 23);
-
-        //FirebaseDatabase.getInstance().getReference().child("FireBaseTriDevDemo").child("Android").setValue("test!");
     }
 
-    public void onClick(View view) {
-        try {
-            TextView text = findViewById(R.id.textDemo);
-            text.setText("Button clicked " + ++clicks + " times");
+    private void showData(DataSnapshot snapshot) {
+        int i = 0;
+        for(DataSnapshot data:snapshot.getChildren()) {
+            UserInfo user = new UserInfo();
+            user.setName(snapshot.child("" + ++i).getValue(UserInfo.class).getName());
+            user.setEmail(snapshot.child("" + ++i).getValue(UserInfo.class).getEmail());
+
+            Log.d(TAG, "Name: " + user.getName());
+            Log.d(TAG, "Email: " + user.getEmail());
+
+            ArrayList<String> arr = new ArrayList<>();
+            arr.add(user.getName());
+            arr.add(user.getEmail());
+            ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, arr);
+            mListView.setAdapter(adapter);
         }
-        catch(Exception e) {
-            Log.d("Error", e.toString());
-        }
     }
 
-    public void newUser(String name, int followers) {
-        PlaceHolder u = new PlaceHolder(name,followers);
-        fireBase.child("users").setValue(u);
-    }
 }
---
+++ UserInfo.java
@@ -0,0 +1,25 @@
+package com.example.getuserinput;
+
+public class UserInfo {
+    private String name,email;
+
+    public UserInfo() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
